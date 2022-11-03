0734871f51adb7bb3e764e2df4dd4899819ff93f
ricardo4466 <rteixeira@mkmservice.com>
2022-11-03 07:15:55 -0300

    create context of aplication and screen signIn

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { NativeBaseProvider, StatusBar } from "native-base"
+import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading'
 import { SignIn } from './src/screens/SignIn'
 
 import { THEME } from './src/styles/theme'
+import { AuthContextProvider } from './src/contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -12,12 +13,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
