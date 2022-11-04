2a8e9d72285f9487feca4b70a74cd3138a7468a2
ricardo4466 <rteixeira@mkmservice.com>
2022-11-04 15:04:12 -0300

    integrating application with the backend

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native'
+
+import { Box } from 'native-base'
+import { SignIn } from '../screens/SignIn'
+import { useAuth } from '../hooks/useAuth'
 import { AppRoutes } from './app.routes'
 
 export function Routes() {
+  const { user } = useAuth()
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg={'gray.900'}>
+      <NavigationContainer>
+        {user.name
+          ? <AppRoutes />
+          : <SignIn />
+        }
+      </NavigationContainer>
+    </Box>
   )
 }
