bd24e0704525a0486a5f488abe7404969d48aad4
ricardo4466 <rteixeira@mkmservice.com>
2022-11-04 12:24:16 -0300

    create routes of navigation in screens

--- App.tsx
+++ App.tsx
@@ -2,15 +2,13 @@ import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading'
-import { Pools } from './src/screens/Pools'
 
 import { THEME } from './src/styles/theme'
 import { AuthContextProvider } from './src/contexts/AuthContext'
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
-
-
   return (
     <NativeBaseProvider theme={THEME}>
       <AuthContextProvider>
@@ -19,7 +17,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
